734763d3bd65d336e1d0c8425d427cbcbe23d31d
jamf <rahu17ec105@rmkcet.ac.in>
2022-11-12 12:20:18 +0530

    adding swiper for project section

--- components/projects/project.tsx
+++ components/projects/project.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './project.module.scss';
 import Image from 'next/image';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Pagination } from 'swiper';
 import path from '../../assets/path.svg';
 import codeEditor from '../../assets/codeEditor.jpg';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Swiper as SwiperClass } from 'swiper/types';
 const Projects: React.FC = () => {
+  const [projectProgress, setProjectProgress] = useState(0);
+  const swiperRef = useRef<SwiperClass>();
   const projectDetails = [
     {
       title: 'Code Editor',
@@ -12,13 +19,13 @@ const Projects: React.FC = () => {
       link: 'https://rahulcool1910.github.io/codeEditor/',
       imageSrc: codeEditor,
     },
-    // {
-    //   title: 'Pathfinder',
-    //   description:
-    //     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, fugiat quod tempora similique temporibus officiis officia ducimus asperiores ullam veritatis! Earum asperiores eum voluptatum soluta corporis odit dolor repudiandae eligendi.',
-    //   link: 'https://rahulcool1910.github.io/pathfinding-visulaizer/',
-    //   imageSrc: codeEditor,
-    // },
+    {
+      title: 'Pathfinder',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, fugiat quod tempora similique temporibus officiis officia ducimus asperiores ullam veritatis! Earum asperiores eum voluptatum soluta corporis odit dolor repudiandae eligendi.',
+      link: 'https://rahulcool1910.github.io/pathfinding-visulaizer/',
+      imageSrc: codeEditor,
+    },
   ];
   return (
     <div className={styles.project_container}>
@@ -27,10 +34,16 @@ const Projects: React.FC = () => {
         <h2 className={styles.project_header_text}>My Portfolio</h2>
         <span>{`</h2>`}</span>
       </div>
-      <div className={styles.project_details}>
+      <Swiper
+        className={styles.project_details}
+        onSlideChange={(index) => setProjectProgress(index.progress)}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+      >
         {projectDetails.map((details, idx) => {
           return (
-            <div className={styles.project_detail}>
+            <SwiperSlide className={styles.project_detail}>
               <Image
                 src={details.imageSrc}
                 alt={''}
@@ -44,10 +57,17 @@ const Projects: React.FC = () => {
                 </span>
                 <span className={styles.project_detail_tags}>{`</h2>`}</span>
               </div>
-            </div>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
+      <button onClick={() => swiperRef.current?.slideNext()}>
+        Slide to the next slide
+      </button>
+      {projectProgress}
+      <button onClick={() => swiperRef.current?.slidePrev()}>
+        Slide to the prev slide
+      </button>
     </div>
   );
 };
