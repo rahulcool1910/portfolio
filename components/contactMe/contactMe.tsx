220ac148b6b9b06ca5ed6b4b295aef46193b0cea
jamf <rahu17ec105@rmkcet.ac.in>
2023-01-11 23:12:07 +0530

    adding links to contact me page

--- components/contactMe/contactMe.tsx
+++ components/contactMe/contactMe.tsx
@@ -1,30 +1,44 @@
 import React, { LegacyRef, useEffect, useRef, useState } from 'react';
 import styles from './contactMe.module.scss';
 
+interface ILink {
+  name: string;
+  link: string;
+  class: string;
+}
 const ContactMe = () => {
-  const links = [
+  const links: ILink[] = [
     {
       name: 'Gmail',
-      link: '',
+      link: 'rahulr1910r@gmail.com',
       class: 'top-[25%] left-[20%] !h-[180px] !w-[180px]',
     },
     {
       name: 'LinkedIn',
-      link: '',
+      link: 'https://www.linkedin.com/in/rahulr19/',
       class: 'top-[10%] right-[25%] !h-[210px] !w-[210px]',
     },
     {
       name: 'Instagram',
-      link: '',
+      link: 'https://www.instagram.com/rahul_rahul19/',
       class: 'top-[55%] right-[15%]',
     },
     {
       name: 'Twitter',
-      link: '',
+      link: 'https://www.instagram.com/rahul_rahul19/',
       class: 'top-[50%] left-[40%] !h-[150px] !w-[150px]',
     },
   ];
   useEffect(() => {}, []);
+
+  const redirectOnClick = (option: ILink) => {
+    if (option.name === 'Gmail') {
+      const email = 'rahulr1910r@gmail.com';
+      document.location = 'mailto:' + email;
+      return;
+    }
+    window.open(option.link, '_blank');
+  };
 
   return (
     <div className={styles.contact_container + ' ContactMeContainer'}>
@@ -39,7 +53,7 @@ const ContactMe = () => {
             <div
               className={styles.contact_links + ` ${link.class}`}
               key={link.name}
-              onClick={console.log}
+              onClick={() => redirectOnClick(link)}
             >
               <span>{`<>`}</span>
               <h2>{link.name}</h2>
